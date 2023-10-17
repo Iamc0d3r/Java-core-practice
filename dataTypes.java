056c89d66bccf41469a626686ac2db4ccd593253
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-17 21:51:33 +0530

    dataTypes.java

---
+++ dataTypes.java
@@ -0,0 +1,24 @@
+// Java Program to Demonstrate Char Primitive Data Type 
+
+
+class dataTypes { 
+
+	public static void main(String args[]) 
+	{ 
+		char a = 'v'; 
+		int i = 69; 
+		byte b = 6; 
+		short s = 46; 
+		double d =5.355453532; 
+		float f = 3.7333434f; 
+		long l = 12121; 
+
+		System.out.println("char: " + a); 
+		System.out.println("integer: " + i); 
+		System.out.println("byte: " + b); 
+		System.out.println("short: " + s); 
+		System.out.println("float: " + f); 
+		System.out.println("double: " + d); 
+		System.out.println("long: " + l); 
+	} 
+}
