2aec00fa295a6243d08e63e5ee0c8852de066476
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-18 20:49:20 +0530

    SwitchCase.java

---
+++ switchCase.java
@@ -0,0 +1,26 @@
+class switchCase{
+	public static void main(String[] args){
+		int a = 5;
+		switch (a){
+		case 1 :
+		System.out.println("found match");
+		break;
+		case 2 :
+		System.out.println("found match");
+		break;
+		case 3 :
+		System.out.println("found match");
+		break;
+		case 4 :
+		System.out.println("found match");
+		break;
+		case 5 :
+		System.out.println("found match");
+		break;
+		
+		default :
+		System.out.println("sorry mate, not found");
+		}
+	}
+	
+}
