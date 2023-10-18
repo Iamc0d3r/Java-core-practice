f617f95a269bdffd3793e5571629227b640d39a6
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-18 20:54:08 +0530

    while loop

---
+++ whileLoop.java
@@ -0,0 +1,10 @@
+class whileLoop {
+	public static void main(String[] args){
+		
+		int a = 0;
+		
+		while(a < 5){
+			System.out.println(a++);  // we are directly incrementing a for the loop and when a reach the condition loop stops 
+		}
+	}
+}
