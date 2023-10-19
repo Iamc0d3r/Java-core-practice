0868710e13b4fd44b07ca46d87ad60eb55d30e7e
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-19 21:56:40 +0530

    Update forLoop.java

--- forLoop.java
+++ forLoop.java
@@ -1,6 +1,6 @@
 class forLoop{
 	public static void main(String[] args){
-		for(int i = 0; i < 5; i++){
+		for(int i = 0; i < 5; i++){  // in for loop we provide three statement initial value , final condition and increment and based on the that statements loop works
 			System.out.println("the value for i is : " + i);
 		}
 	}
