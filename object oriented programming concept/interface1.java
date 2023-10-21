a4481ad92b3822735de1a42a5d5308b73a3a900c
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-21 21:22:44 +0530

    Update interface1.java

--- object oriented programming concept/interface1.java
+++ object oriented programming concept/interface1.java
@@ -1,9 +1,10 @@
 interface inter1{
 	public abstract void m1();
+		 //m1()  method does not have any implementation because it is an abstract method.
 }
 class a implements inter1{
 
-	public void m1(){
+	public void m1(){		//m1() method gets imlementation after interface implementation.
 		System.out.println("this is a interface concept");
 	}
 	}
