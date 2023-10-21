cc330c82aeffc4b1ee518670926b24e8c1390545
Vishal <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-21 21:20:04 +0530

    Update inherit1.java

--- object oriented programming concept/inherit1.java
+++ object oriented programming concept/inherit1.java
@@ -1,9 +1,11 @@
-class parent{
+class parent{  
+		 //parent class having method home().
 	public void home(){
 		System.out.println("We have home.");
 	}
 }
 class child extends parent{
+		//child class inherit home() method and now has 2 methods home() and car().
 	public void car(){
 		System.out.println("we have car.");
 	}
